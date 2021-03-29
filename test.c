80c0e3b3deffe6a8604d4f133f52166d0a9d4a39
Vasiliy Vylegzhanin <v.gadfly@gmail.com>
2021-03-29 18:33:31 +0300

    test: vector ser

--- test.c
+++ test.c
@@ -44,7 +44,7 @@ int main( int argc, char *argv[] )
     printf("\n");
     
     g_free(bytes);
-/*
+
     WainThree *three = g_object_new( WAIN_TYPE_THREE, NULL );
     GList *list = NULL;
     gint32 *pi = g_new(gint32, 1);
@@ -57,13 +57,24 @@ int main( int argc, char *argv[] )
     *pi = 3;
     list = g_list_append( list, pi );
     three->vi = list;
+
     list = g_list_append( NULL, two );
     three->vo = list;
+
     list = g_list_append( NULL, "s_one" );
     list = g_list_append( list, "s_two" );
     three->vs = list;
-    printf( "== Three ==\n" );
-    wain_object_serialize( three, ds );
-*/
+    
+    len = wain_object_length( three );
+    printf("Three serialized len is %d\n", len);
+    bytes = g_malloc(len);
+    wain_object_serialize( three, bytes );
+    
+    for ( i = 0; i < len; i+=4 )
+        printf("%08X.", *(guint32*)(bytes+i));
+    printf("\n");
+    
+    g_free(bytes);
+
     return 0;
 }
